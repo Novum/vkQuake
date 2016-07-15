b0eb65b777b3baa70f8612280c8bfcdebfab6c83
Axel Gneiting <axel.gneiting@idsoftware.com>
2016-07-15 11:52:15 -0500

    - Support older compiler in shader compile batch

--- Shaders/compile.bat
+++ Shaders/compile.bat
@@ -1,5 +1,11 @@
 @echo off
+if exist "%VS140COMNTOOLS%\VsDevCmd.bat" ( 
 call "%VS140COMNTOOLS%\VsDevCmd.bat"
+) else if exist "%VS120COMNTOOLS%\VsDevCmd.bat" ( 
+call "%VS120COMNTOOLS%\VsDevCmd.bat"
+) else if exist "%VS110COMNTOOLS%\VsDevCmd.bat" ( 
+call "%VS110COMNTOOLS%\VsDevCmd.bat"
+)
 cl.exe /nologo bintoc.c 
 %VULKAN_SDK%\bin\glslangValidator.exe -V basic.vert -o basic.vspv
 %VULKAN_SDK%\bin\glslangValidator.exe -V basic.frag -o basic.fspv
