3c7cde8b7eec93bf3aeb37d4da4c4d51a701b843
Axel Gneiting <mail@axelgneiting.de>
2017-08-04 22:00:52 -0500

    in_sdl.c: raise joy_deadzone_trigger to 0.2.
    Previous value of 0.001 was probably too low, may have caused
    "stuck" triggers:
    http://celephais.net/board/view_thread.php?id=60452&start=2935
    
    git-svn-id: https://svn.code.sf.net/p/quakespasm/code/trunk@1439 af15c1b1-3010-417e-b628-4374ebc0bcbd
    
    # Conflicts:
    #	quakespasm/Linux/sgml/Quakespasm.sgml
    #	quakespasm/Quakespasm.html
    #	quakespasm/Quakespasm.txt

--- Quake/in_sdl.c
+++ Quake/in_sdl.c
@@ -34,7 +34,7 @@ static cvar_t in_debugkeys = {"in_debugkeys", "0", CVAR_NONE};
 
 // SDL2 Game Controller cvars
 cvar_t	joy_deadzone = { "joy_deadzone", "0.175", CVAR_ARCHIVE };
-cvar_t	joy_deadzone_trigger = { "joy_deadzone_trigger", "0.001", CVAR_ARCHIVE };
+cvar_t	joy_deadzone_trigger = { "joy_deadzone_trigger", "0.2", CVAR_ARCHIVE };
 cvar_t	joy_sensitivity_yaw = { "joy_sensitivity_yaw", "300", CVAR_ARCHIVE };
 cvar_t	joy_sensitivity_pitch = { "joy_sensitivity_pitch", "150", CVAR_ARCHIVE };
 cvar_t	joy_invert = { "joy_invert", "0", CVAR_ARCHIVE };
