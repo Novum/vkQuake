ade17a035acf4f77f57929c21caecc202daaf535
sezero <sezero@users.sourceforge.net>
2017-10-20 19:34:33 +0300

    Sys_Error if SDL2 version < 2.0.6

--- Quake/main_sdl.c
+++ Quake/main_sdl.c
@@ -29,10 +29,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 #include <stdio.h>
 
-/* need at least SDL_2.0.0 */
+/* need at least SDL_2.0.6 */
 #define SDL_MIN_X	2
 #define SDL_MIN_Y	0
-#define SDL_MIN_Z	0
+#define SDL_MIN_Z	6
 #define SDL_REQUIREDVERSION	(SDL_VERSIONNUM(SDL_MIN_X,SDL_MIN_Y,SDL_MIN_Z))
 #define SDL_NEW_VERSION_REJECT	(SDL_VERSIONNUM(3,0,0))
 
@@ -53,7 +53,7 @@ static void Sys_InitSDL (void)
 		Sys_Error("You need at least v%d.%d.%d of SDL to run this game.", SDL_MIN_X,SDL_MIN_Y,SDL_MIN_Z);
 	}
 	if (SDL_VERSIONNUM(sdl_version->major,sdl_version->minor,sdl_version->patch) >= SDL_NEW_VERSION_REJECT)
-	{	/*reject running under newer (1.3.x) SDL */
+	{	/*reject running under newer (3.x) SDL */
 		Sys_Error("Your version of SDL library is incompatible with me.\n"
 			  "You need a library version in the line of %d.%d.%d\n", SDL_MIN_X,SDL_MIN_Y,SDL_MIN_Z);
 	}
