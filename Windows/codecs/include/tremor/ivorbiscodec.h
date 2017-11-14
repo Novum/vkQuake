573fad8f62fe80c3950ddec4636819da2102c05e
Axel Gneiting <mail@axelgneiting.de>
2017-11-13 21:10:24 -0600

    oslibs:  add missing tremor builds for windows.
    git-svn-id: https://svn.code.sf.net/p/quakespasm/code/trunk@1530 af15c1b1-3010-417e-b628-4374ebc0bcbd

--- Windows/codecs/include/tremor/ivorbiscodec.h
+++ Windows/codecs/include/tremor/ivorbiscodec.h
@@ -153,9 +153,6 @@ extern void     vorbis_info_init(vorbis_info *vi);
 extern void     vorbis_info_clear(vorbis_info *vi);
 extern int      vorbis_info_blocksize(vorbis_info *vi,int zo);
 extern void     vorbis_comment_init(vorbis_comment *vc);
-extern void     vorbis_comment_add(vorbis_comment *vc, char *comment); 
-extern void     vorbis_comment_add_tag(vorbis_comment *vc, 
-				       char *tag, char *contents);
 extern char    *vorbis_comment_query(vorbis_comment *vc, char *tag, int count);
 extern int      vorbis_comment_query_count(vorbis_comment *vc, char *tag);
 extern void     vorbis_comment_clear(vorbis_comment *vc);
