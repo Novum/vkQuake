8cf7bb4f056a071427b0c838edf27d7d5c6aa302
sezero <sezero@af15c1b1-3010-417e-b628-4374ebc0bcbd>
2018-04-27 17:30:38 +0000

    bgmusic.c: mark *.opus files as cdrip types.
    git-svn-id: https://svn.code.sf.net/p/quakespasm/code/trunk@1579 af15c1b1-3010-417e-b628-4374ebc0bcbd
    
    # Conflicts:
    #	quakespasm/Linux/sgml/Quakespasm.sgml
    #	quakespasm/Quakespasm.html
    #	quakespasm/Quakespasm.txt

--- Quake/bgmusic.c
+++ Quake/bgmusic.c
@@ -3,7 +3,7 @@
  * Handles streaming music as raw sound samples and runs the midi driver
  *
  * Copyright (C) 1999-2005 Id Software, Inc.
- * Copyright (C) 2010-2012 O.Sezer <sezero@users.sourceforge.net>
+ * Copyright (C) 2010-2018 O.Sezer <sezero@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -69,7 +69,7 @@ static music_handler_t wanted_handlers[] =
 static music_handler_t *music_handlers = NULL;
 
 #define ANY_CODECTYPE	0xFFFFFFFF
-#define CDRIP_TYPES	(CODECTYPE_VORBIS | CODECTYPE_MP3 | CODECTYPE_FLAC | CODECTYPE_WAV)
+#define CDRIP_TYPES	(CODECTYPE_VORBIS | CODECTYPE_MP3 | CODECTYPE_FLAC | CODECTYPE_WAV | CODECTYPE_OPUS)
 #define CDRIPTYPE(x)	(((x) & CDRIP_TYPES) != 0)
 
 static snd_stream_t *bgmstream = NULL;
