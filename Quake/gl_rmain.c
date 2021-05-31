e2d6c49b2333ae77015a034931a0f6dcaa850280
Axel Gneiting <axelgneiting@gmail.com>
2021-05-30 22:30:37 -0500

    Fix dynamic lights not updating properly

--- Quake/gl_rmain.c
+++ Quake/gl_rmain.c
@@ -318,7 +318,6 @@ void R_SetupScene (void)
 	render_pass_index = 0;
 	vkCmdBeginRenderPass(vulkan_globals.command_buffer, &vulkan_globals.main_render_pass_begin_infos[render_warp ? 1 : 0], VK_SUBPASS_CONTENTS_INLINE);
 
-	r_framecount++;
 	R_SetupMatrix ();
 }
 
@@ -332,6 +331,7 @@ void R_SetupView (void)
 	// Need to do those early because we now update dynamic light maps during R_MarkSurfaces
 	R_PushDlights();
 	R_AnimateLight ();
+	r_framecount++;
 
 	Fog_SetupFrame (); //johnfitz
 
