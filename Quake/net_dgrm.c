Novum/vkQuake

commit 40186bd0d3fa1f766bc082140d438ef17b4618f6

more -Wshadow fixes -- this time in net stuff

Quake/net_dgrm.c
@@ -1176,13 +1176,13 @@ static void _Datagram_ServerControlPacket (sys_socket_t acceptsock, struct qsock
 			//masters only really need gamename and player counts. actual clients might want player names too.
 			qboolean full = !strcmp(Cmd_Argv(0), "getstatus");
 			char cookie[128];
-			const char *s = Cmd_Args();
+			const char *str = Cmd_Args();
 			const char *gamedir = COM_GetGameNames(false);
 			unsigned int numclients = 0, numbots = 0;
 			int i;
 			size_t j;
-			if (!s) s = "";
-			q_strlcpy(cookie, s, sizeof(cookie));
+			if (!str) str = "";
+			q_strlcpy(cookie, str, sizeof(cookie));
 
 			for (i = 0; i < svs.maxclients; i++)
 			{
@@ -1769,7 +1769,7 @@ static qboolean _Datagram_SearchForHosts (qboolean xmit)
 			if (msg_readcount+19 <= net_message.cursize && !strncmp((char*)net_message.data+msg_readcount, "getserversResponse", 18))
 			{
 				struct qsockaddr addr;
-				int i;
+				int j;
 				msg_readcount += 18;
 				for(;;)
 				{
@@ -1778,8 +1778,8 @@ static qboolean _Datagram_SearchForHosts (qboolean xmit)
 					case '\\':
 						memset(&addr, 0, sizeof(addr));
 						addr.qsa_family = AF_INET;
-						for (i = 0; i < 4; i++)
-							((byte*)&((struct sockaddr_in*)&addr)->sin_addr)[i] = MSG_ReadByte();
+						for (j = 0; j < 4; i++)
+							((byte*)&((struct sockaddr_in*)&addr)->sin_addr)[j] = MSG_ReadByte();
 						((byte*)&((struct sockaddr_in*)&addr)->sin_port)[0] = MSG_ReadByte();
 						((byte*)&((struct sockaddr_in*)&addr)->sin_port)[1] = MSG_ReadByte();
 						if (!((struct sockaddr_in*)&addr)->sin_port)
@@ -1788,8 +1788,8 @@ static qboolean _Datagram_SearchForHosts (qboolean xmit)
 					case '/':
 						memset(&addr, 0, sizeof(addr));
 						addr.qsa_family = AF_INET6;
-						for (i = 0; i < 16; i++)
-							((byte*)&((struct sockaddr_in6*)&addr)->sin6_addr)[i] = MSG_ReadByte();
+						for (j = 0; j < 16; i++)
+							((byte*)&((struct sockaddr_in6*)&addr)->sin6_addr)[j] = MSG_ReadByte();
 						((byte*)&((struct sockaddr_in6*)&addr)->sin6_port)[0] = MSG_ReadByte();
 						((byte*)&((struct sockaddr_in6*)&addr)->sin6_port)[1] = MSG_ReadByte();
 						if (!((struct sockaddr_in6*)&addr)->sin6_port)

Quake/Makefile.w64
@@ -44,7 +44,7 @@ STRIP = strip
 CPUFLAGS=
 LDFLAGS = -m64 -mwindows
 DFLAGS ?=
-CFLAGS ?= -m64 -Wall -Wno-trigraphs -std=gnu99 -D_WIN32_WINNT=0x0501
+CFLAGS ?= -m64 -Wall -Wno-trigraphs -std=gnu99 -D_WIN32_WINNT=0x0501 -Wshadow
 CFLAGS += $(CPUFLAGS)
 
 ifneq ($(DEBUG),0)

Quake/net_udp.c
@@ -456,8 +456,8 @@ int UDP_GetSocketAddr (sys_socket_t socketid, struct qsockaddr *addr)
 	}
 	else if (addr->qsa_family == AF_INET6)
 	{
-		static const struct in6_addr in6addr_any = IN6ADDR_ANY_INIT;
-		if (!memcmp(&((struct sockaddr_in6 *)addr)->sin6_addr, &in6addr_any, sizeof(in6addr_any)))
+		static const struct in6_addr _in6addr_any = IN6ADDR_ANY_INIT;
+		if (!memcmp(&((struct sockaddr_in6 *)addr)->sin6_addr, &_in6addr_any, sizeof(_in6addr_any)))
 			memcpy(&((struct sockaddr_in6 *)addr)->sin6_addr, &myAddrv6, sizeof(((struct sockaddr_in6 *)addr)->sin6_addr));
 	}
 

Quake/net_wins.c
@@ -518,8 +518,8 @@ int WINS_GetSocketAddr (sys_socket_t socketid, struct qsockaddr *addr)
 #ifdef IPPROTO_IPV6
 	if (addr->qsa_family == AF_INET6)
 	{
-		static const in_addr6_t in6addr_any;// = IN6ADDR_ANY_INIT;
-		if (!memcmp(&((struct sockaddr_in6 *)addr)->sin6_addr, &in6addr_any, sizeof(in_addr6_t)))
+		static const in_addr6_t _in6addr_any;// = IN6ADDR_ANY_INIT;
+		if (!memcmp(&((struct sockaddr_in6 *)addr)->sin6_addr, &_in6addr_any, sizeof(in_addr6_t)))
 			memcpy(&((struct sockaddr_in6 *)addr)->sin6_addr, &myAddrv6, sizeof(in_addr6_t));
 	}
 #endif
@@ -553,11 +553,11 @@ int	WINIPv4_GetAddresses (qhostaddr_t *addresses, int maxaddresses)
 	if (bindAddrv4 == INADDR_ANY)
 	{
 		//on windows, we can just do a dns lookup on our own hostname and expect an ipv4 result
-		char		hostname[64];
+		char		buf[64];
 		u_long		addr;
-		gethostname(hostname, sizeof(hostname));
+		gethostname(buf, sizeof(buf));
 
-		h = gethostbyname(hostname);
+		h = gethostbyname(buf);
 		if(h && h->h_addrtype == AF_INET)
 		{
 			for (b = 0; h->h_addr_list[b] && result < maxaddresses; b++)
@@ -580,15 +580,15 @@ int	WINIPv6_GetAddresses (qhostaddr_t *addresses, int maxaddresses)
 	{
 		//on windows, we can just do a dns lookup on our own hostname and expect an ipv4 result
 		struct addrinfo hints, *addrlist, *itr;
-		char		hostname[64];
+		char		buf[64];
 		memset(&hints, 0, sizeof(struct addrinfo));
 		hints.ai_family = AF_INET6;    /* Allow IPv4 or IPv6 */
 		hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
 		hints.ai_flags = 0;
 		hints.ai_protocol = 0;          /* Any protocol */
 
-		gethostname(hostname, sizeof(hostname));
-		if (qgetaddrinfo(hostname, NULL, &hints, &addrlist) == 0)
+		gethostname(buf, sizeof(buf));
+		if (qgetaddrinfo(buf, NULL, &hints, &addrlist) == 0)
 		{
 			for (itr = addrlist; itr && result < maxaddresses; itr = itr->ai_next)
 			{

commit 4611d7390680f8e7199c2e85f0324b4629498807

fixed breakage I introduced in commit 40186bd0d3fa1

Quake/net_dgrm.c
@@ -1778,7 +1778,7 @@ static qboolean _Datagram_SearchForHosts (qboolean xmit)
 					case '\\':
 						memset(&addr, 0, sizeof(addr));
 						addr.qsa_family = AF_INET;
-						for (j = 0; j < 4; i++)
+						for (j = 0; j < 4; j++)
 							((byte*)&((struct sockaddr_in*)&addr)->sin_addr)[j] = MSG_ReadByte();
 						((byte*)&((struct sockaddr_in*)&addr)->sin_port)[0] = MSG_ReadByte();
 						((byte*)&((struct sockaddr_in*)&addr)->sin_port)[1] = MSG_ReadByte();
@@ -1788,7 +1788,7 @@ static qboolean _Datagram_SearchForHosts (qboolean xmit)
 					case '/':
 						memset(&addr, 0, sizeof(addr));
 						addr.qsa_family = AF_INET6;
-						for (j = 0; j < 16; i++)
+						for (j = 0; j < 16; j++)
 							((byte*)&((struct sockaddr_in6*)&addr)->sin6_addr)[j] = MSG_ReadByte();
 						((byte*)&((struct sockaddr_in6*)&addr)->sin6_port)[0] = MSG_ReadByte();
 						((byte*)&((struct sockaddr_in6*)&addr)->sin6_port)[1] = MSG_ReadByte();