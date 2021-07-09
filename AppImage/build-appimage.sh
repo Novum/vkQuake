79a7fed86fba2ef6491ef9f23e8f59773011439b
Axel Gneiting <axelgneiting@gmail.com>
2021-07-09 00:30:26 -0500

    Fix AppImage creation. Previous script didn't actually produce binaries that mounted squashfs.

--- AppImage/build-appimage.sh
+++ AppImage/build-appimage.sh
@@ -2,6 +2,4 @@
 make -C ../Quake clean
 make -C ../Quake -j
 rm -rf AppDir
-./linuxdeploy-x86_64.AppImage -e ../Quake/vkquake --appdir=AppDir --create-desktop-file -i ../Misc/vkQuake_256.png --icon-filename=vkquake
-rm vkquake-x86_64.AppImage
-./appimagetool-x86_64.AppImage AppDir --runtime-file AppDir/usr/bin/vkquake
+./linuxdeploy-x86_64.AppImage -e ../Quake/vkquake --appdir=AppDir --create-desktop-file -i ../Misc/vkQuake_256.png --icon-filename=vkquake --output appimage
