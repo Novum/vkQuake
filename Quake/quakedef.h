079d83a66695ae91eedfd80ab553dc18bf893070
Guillaume Plourde <gplourde@protonmail.com>
2021-08-21 22:31:31 -0400

    Nightdive RERELEASE fix crashes

--- Quake/quakedef.h
+++ Quake/quakedef.h
@@ -272,6 +272,8 @@ typedef struct
 #include "glquake.h"
 #include "../Shaders/shaders.h"
 
+#include "nightdive.h"
+
 //=============================================================================
 
 // the host system specifies the base of the directory tree, the
