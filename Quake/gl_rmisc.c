a6617f7e2e0333b3628783da471f522f487fa3ec
Axel Gneiting <axelgneiting@gmail.com>
2021-09-02 21:17:41 -0700

    Fix timerefresh crashing
    Not sure how useful this command actually is

--- Quake/gl_rmisc.c
+++ Quake/gl_rmisc.c
@@ -2676,7 +2676,7 @@ void R_TimeRefresh_f (void)
 		GL_BeginRendering(&glx, &gly, &glwidth, &glheight);
 		r_refdef.viewangles[1] = i/128.0*360.0;
 		R_RenderView ();
-		GL_EndRendering (true);
+		GL_EndRendering (false);
 	}
 
 	//glFinish ();
