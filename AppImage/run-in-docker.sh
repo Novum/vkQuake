e1359e961b401920cf083984c7ee841d6f9e2e81
Axel Gneiting <axelgneiting@gmail.com>
2021-10-25 22:12:43 -0700

    Adjust AppImage file name in archive

--- AppImage/run-in-docker.sh
+++ AppImage/run-in-docker.sh
@@ -8,6 +8,6 @@ rm -rf vkquake_linux64
 rm -f vkquake_linux64.tar.gz
 mkdir vkquake_linux64
 ./linuxdeploy-x86_64.AppImage -e ../Quake/vkquake --appdir=AppDir --create-desktop-file -i ../Misc/vkQuake_256.png --icon-filename=vkquake --output appimage
-cp vkquake-x86_64.AppImage vkquake_linux64
+cp vkquake-x86_64.AppImage vkquake_linux64/vkquake.AppImage
 cp ../Quake/vkquake.pak vkquake_linux64
 tar -zcvf vkquake_linux64.tar.gz vkquake_linux64
