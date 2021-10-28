27fafb851d332129550bfe173c70903cc0267e6a
Axel Gneiting <axelgneiting@gmail.com>
2021-10-28 00:19:42 -0700

    Add LICENSE.txt to archive and add git hash to name

--- AppImage/run-in-docker.sh
+++ AppImage/run-in-docker.sh
@@ -4,10 +4,12 @@ cd /usr/src/vkQuake/AppImage
 make -C ../Quake clean
 make -C ../Quake -j
 rm -rf AppDir
-rm -rf vkquake_linux64
-rm -f vkquake_linux64.tar.gz
-mkdir vkquake_linux64
+FOLDER=vkquake${VERSION}_linux64
+ARCHIVE=${FOLDER}.tar.gz
+rm -rf vkquake*
+mkdir ${FOLDER}
 ./linuxdeploy-x86_64.AppImage -e ../Quake/vkquake --appdir=AppDir --create-desktop-file -i ../Misc/vkQuake_256.png --icon-filename=vkquake --output appimage
-cp vkquake-x86_64.AppImage vkquake_linux64/vkquake.AppImage
-cp ../Quake/vkquake.pak vkquake_linux64
-tar -zcvf vkquake_linux64.tar.gz vkquake_linux64
+cp vkquake-x86_64.AppImage ${FOLDER}/vkquake.AppImage
+cp ../Quake/vkquake.pak ${FOLDER}
+cp ../LICENSE.txt ${FOLDER}
+tar -zcvf ${ARCHIVE} ${FOLDER}
