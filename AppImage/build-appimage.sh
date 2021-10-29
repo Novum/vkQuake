a3d6e2e8ad0d3549234b687a0b333e3ee27a594e
Axel Gneiting <axelgneiting@gmail.com>
2021-10-28 22:07:41 -0700

    More CI fixes

--- AppImage/build-appimage.sh
+++ AppImage/build-appimage.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 docker build --tag=build-vkquake docker
-docker run --rm --privileged -e -v ${PWD}/..:/usr/src/vkQuake build-vkquake /usr/src/vkQuake/AppImage/run-in-docker.sh
+docker run --rm --privileged -e VERSION=`./get-version.sh` -v ${PWD}/..:/usr/src/vkQuake build-vkquake /usr/src/vkQuake/AppImage/run-in-docker.sh
