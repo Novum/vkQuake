375db68bdf7ebec96d831e429d8cb36b2f28d400
Axel Gneiting <axelgneiting@gmail.com>
2021-11-25 09:28:10 -0800

    Fix CI not failing on compile errors

--- AppImage/run-in-docker.sh
+++ AppImage/run-in-docker.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-modprobe fuse
-cd /usr/src/vkQuake/AppImage
-make -C ../Quake clean
-make -C ../Quake -j
-rm -rf AppDir
 FOLDER=vkquake-${VERSION}_linux64
 ARCHIVE=${FOLDER}.tar.gz
-rm -rf vkquake*
-mkdir ${FOLDER}
-./linuxdeploy-x86_64.AppImage -e ../Quake/vkquake --appdir=AppDir --create-desktop-file -i ../Misc/vkQuake_256.png --icon-filename=vkquake --output appimage
-cp vkquake-${VERSION}-x86_64.AppImage ${FOLDER}/vkquake.AppImage
-cp ../Quake/vkquake.pak ${FOLDER}
-cp ../LICENSE.txt ${FOLDER}
-tar -zcvf ${ARCHIVE} ${FOLDER}
+
+cd /usr/src/vkQuake/AppImage \
+  && make -C ../Quake clean \
+  && make -C ../Quake -j \
+  && rm -rf AppDir \
+  && rm -rf vkquake* \
+  && mkdir ${FOLDER} \
+  && ./linuxdeploy-x86_64.AppImage -e ../Quake/vkquake --appdir=AppDir --create-desktop-file \
+     -i ../Misc/vkQuake_256.png --icon-filename=vkquake --output appimage \
+  && cp vkquake-${VERSION}-x86_64.AppImage ${FOLDER}/vkquake.AppImage \
+  && cp ../Quake/vkquake.pak ${FOLDER} \
+  && cp ../LICENSE.txt ${FOLDER} \
+  && tar -zcvf ${ARCHIVE} ${FOLDER}
