53bd56d9300c5146819e2c841298d1b7bbec51da
Ozkan Sezer <sezeroz@gmail.com>
2021-11-28 17:10:50 +0300

    upgrade SDL2 to 2.0.18

--- Windows/SDL2/include/SDL_revision.h
+++ Windows/SDL2/include/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "hg-14525:e52d96ea04fc"
-#define SDL_REVISION_NUMBER 14525
+#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@c2904c8ce70cb1c295740ab5d65d36d226ead11f"
+#define SDL_REVISION_NUMBER 0
