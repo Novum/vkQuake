41bdd4bc932210ec357fff80c27cd79800c29a5f
Ozkan Sezer <sezeroz@gmail.com>
2021-11-29 01:27:40 +0300

    newer windows SDL2-2.0.18 build,
    fixing mainstream SDL bug 5026 .

--- Windows/SDL2/include/SDL_revision.h
+++ Windows/SDL2/include/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@c2904c8ce70cb1c295740ab5d65d36d226ead11f"
+#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@942973dd86a868ddc08e390d776f7db3bb2e7cea"
 #define SDL_REVISION_NUMBER 0
