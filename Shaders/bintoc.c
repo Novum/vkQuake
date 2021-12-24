10089eb69237c82733007fd3027dabd692bb0ed2
Axel Gneiting <axelgneiting@gmail.com>
2021-12-24 11:42:54 -0800

    Compile shaders in solution
    Also change all shaders to unix file endings

--- Shaders/bintoc.c
+++ Shaders/bintoc.c
@@ -2,25 +2,32 @@
 #include <assert.h>
 
 int main(int argc, char** argv) {
-    if(argc != 3)
+	if(argc != 4)
 	return 0;
 
-    char* fn = argv[1];
-    FILE* f = fopen(fn, "rb");
-    printf("unsigned char %s[] = {\n", argv[2]);
-    unsigned long n = 0;
+	char* fnin = argv[1];
+	FILE* fin = fopen(fnin, "rb");
 
-    while(!feof(f)) {
-        unsigned char c;
-        if(fread(&c, 1, 1, f) == 0) break;
-        printf("0x%.2X, ", (int)c);
-        ++n;
-        if(n % 10 == 0) printf("\n");
-    }
+	char* fnout = argv[3];
+	FILE* fout = fopen(fnout, "w+");
 
-    fclose(f);
-    printf("};\n");
+	for (char* c = argv[2]; *c != 0; ++c)
+		if (*c == '.') *c = '_';
+	fprintf(fout, "unsigned char %s[] = {\n", argv[2]);
+	unsigned long n = 0;
 
-    printf("int %s_size = %ld;\n", argv[2], n);
-    return 0;
+	while(!feof(fin)) {
+		unsigned char c;
+		if(fread(&c, 1, 1, fin) == 0) break;
+		fprintf(fout, "0x%.2X, ", (int)c);
+		++n;
+		if(n % 10 == 0) fprintf(fout, "\n");
+	}
+
+	fclose(fin);
+	fprintf(fout, "};\n");
+
+	fprintf(fout, "int %s_size = %ld;\n", argv[2], n);
+	fclose(fout);
+	return 0;
 }
