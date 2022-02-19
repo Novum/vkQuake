79106cef156ce2fa31a8b03d4e2c149cd0494570
vsonnier <vsonnier@users.noreply.github.com>
2022-02-19 17:55:34 +0100

    Add the missing pieces for UF_TRAILEFFECT and UF_TAGINFO from QSS

--- Quake/cl_parse.c
+++ Quake/cl_parse.c
@@ -463,14 +463,14 @@ static unsigned int CLFTE_ReadDelta (unsigned int entnum, entity_state_t *news, 
 	if (bits & UF_TRAILEFFECT)
 	{
 		unsigned short v = MSG_ReadShort ();
-		/*news->emiteffectnum = 0;*/
-		/*news->traileffectnum = v & 0x3fff;*/
+		news->emiteffectnum = 0;
+		news->traileffectnum = v & 0x3fff;
 		if (v & 0x8000)
-			/*news->emiteffectnum = */ MSG_ReadShort () /*& 0x3fff*/;
-		/*if (news->traileffectnum >= MAX_PARTICLETYPES)
+			news->emiteffectnum = MSG_ReadShort() & 0x3fff;
+		if (news->traileffectnum >= MAX_PARTICLETYPES)
 		    news->traileffectnum = 0;
 		if (news->emiteffectnum >= MAX_PARTICLETYPES)
-		    news->emiteffectnum = 0;*/
+			news->emiteffectnum = 0;
 	}
 
 	if (bits & UF_COLORMOD)
