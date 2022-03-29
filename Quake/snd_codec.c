254c97e2368d71acd4a95b5df24be18d60c4f9e5
Ozkan Sezer <sezeroz@gmail.com>
2022-03-29 17:05:40 +0300

    add back snd_modplug (disabled, for future ref.), update project files.

--- Quake/snd_codec.c
+++ Quake/snd_codec.c
@@ -29,6 +29,7 @@
 
 /* headers for individual codecs */
 #include "snd_mikmod.h"
+#include "snd_modplug.h"
 #include "snd_xmp.h"
 #include "snd_umx.h"
 #include "snd_wave.h"
@@ -64,6 +65,9 @@ void S_CodecInit (void)
 	 * of codec choice preference: */
 #ifdef USE_CODEC_UMX
 	S_CodecRegister (&umx_codec);
+#endif
+#ifdef USE_CODEC_MODPLUG
+	S_CodecRegister (&modplug_codec);
 #endif
 #ifdef USE_CODEC_MIKMOD
 	S_CodecRegister (&mikmod_codec);
