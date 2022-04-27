cf3ff281a81982548b3b998e3f5d057e752096f5
Ozkan Sezer <sezeroz@gmail.com>
2022-04-27 14:51:10 +0300

    SDL2-2.0.22 final release version
    actually, only the git revision number differs, all the rest is the
    same as before, as far as windows is concerned.

--- Windows/SDL2/include/SDL_revision.h
+++ Windows/SDL2/include/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@981e1e3c4489add5bf6d4df5415af3cf1ef2773d"
+#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@53dea9830964eee8b5c2a7ee0a65d6e268dc78a1"
 #define SDL_REVISION_NUMBER 0
