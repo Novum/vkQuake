8609e283dec56440d08549e74746fd1bd84bb98f
Ozkan Sezer <sezeroz@gmail.com>
2022-05-26 11:55:50 +0300

    updated SDL2-2.0.22 builds with several backports from SDL2 main branch.

--- Windows/SDL2/include/SDL_revision.h
+++ Windows/SDL2/include/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@53dea9830964eee8b5c2a7ee0a65d6e268dc78a1"
+#define SDL_REVISION "@56c8381b400ec81732095e429649ce5e74f8cb28"
 #define SDL_REVISION_NUMBER 0
