720b79a9d6891b0b5551335ff70930d081f67d2c
Axel Gneiting <axelgneiting@gmail.com>
2022-07-03 11:24:46 -0700

    Move AppImage to Packaging/AppImage

--- AppImage/build-appimage.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-docker build --tag=build-vkquake docker
-docker run --rm --privileged -e VERSION=`./get-version.sh` -v ${PWD}/..:/usr/src/vkQuake build-vkquake /usr/src/vkQuake/AppImage/run-in-docker.sh
