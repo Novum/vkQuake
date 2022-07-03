12317cfdaf35169ea93db1f28336167f0c8540f9
Axel Gneiting <axelgneiting@gmail.com>
2022-07-03 11:24:46 -0700

    Move AppImage to Packaging/AppImage

---
+++ Packaging/AppImage/run-in-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+FOLDER=vkquake-${VERSION}_linux64
+ARCHIVE=${FOLDER}.tar.gz
+
+cd /usr/src/vkQuake \
+  && ./Packaging/AppImage/python3.11.0.AppImage /opt/meson/meson.py build/appimage -Dbuildtype=release -Db_lto=true \
+  && ninja -C build/appimage \
+  && cd Packaging/AppImage \
+  && rm -rf AppDir \
+  && rm -rf vkquake* \
+  && mkdir ${FOLDER} \
+  && ./linuxdeploy-x86_64.AppImage -e ../../build/appimage/vkquake --appdir=AppDir --create-desktop-file \
+     -i ../../Misc/vkQuake_256.png --icon-filename=vkquake --output appimage \
+  && cp vkquake-${VERSION}-x86_64.AppImage ${FOLDER}/vkquake.AppImage \
+  && cp ../../Quake/vkquake.pak ${FOLDER} \
+  && cp ../../LICENSE.txt ${FOLDER} \
+  && tar -zcvf ${ARCHIVE} ${FOLDER}
