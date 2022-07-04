ab0cddbedb7ee74bdd73a383b83f4c39d2def68a
Axel Gneiting <axelgneiting@gmail.com>
2022-07-03 17:23:54 -0700

    remove accidentally checked in code

--- Quake/tasks.c
+++ Quake/tasks.c
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "atomics.h"
 #include "quakedef.h"
 
-#if 1
+#if defined(USE_HELGRIND)
 #include "valgrind/helgrind.h"
 #else
 #define ANNOTATE_HAPPENS_BEFORE(x) \
