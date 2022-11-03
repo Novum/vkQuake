7568213511c2a16da89250c1eeab3351bac7da3f
temx <nukhead@gmail.com>
2022-11-03 18:10:27 +0100

    Allow exceeding TEXTURE_HEAP_SIZE if necessary

--- Quake/gl_texmgr.c
+++ Quake/gl_texmgr.c
@@ -934,7 +934,7 @@ static void TexMgr_LoadImage32 (gltexture_t *glt, unsigned *data)
 	vkGetImageMemoryRequirements (vulkan_globals.device, glt->image, &memory_requirements);
 
 	uint32_t     memory_type_index = GL_MemoryTypeFromProperties (memory_requirements.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, 0);
-	VkDeviceSize heap_size = TEXTURE_HEAP_SIZE_MB * (VkDeviceSize)1024 * (VkDeviceSize)1024;
+	VkDeviceSize heap_size = q_max (TEXTURE_HEAP_SIZE_MB * (VkDeviceSize)1024 * (VkDeviceSize)1024, memory_requirements.size);
 	VkDeviceSize aligned_offset = GL_AllocateFromHeaps (
 		&num_texmgr_heaps, &texmgr_heaps, heap_size, memory_type_index, VULKAN_MEMORY_TYPE_DEVICE, memory_requirements.size, memory_requirements.alignment,
 		&glt->heap, &glt->heap_node, &num_vulkan_tex_allocations, "Textures Heap");
