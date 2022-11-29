69474e71044101ad3d218a7eaeb06eaa5b9427d0
temx <nukhead@gmail.com>
2022-11-29 02:41:32 +0100

    Clear used_water_surfs on free

--- Quake/gl_model.c
+++ Quake/gl_model.c
@@ -325,6 +325,7 @@ static void Mod_FreeModelMemory (qmodel_t *mod)
 		SAFE_FREE (mod->entities);
 		SAFE_FREE (mod->extradata);
 		SAFE_FREE (mod->water_surfs);
+		mod->used_water_surfs = 0;
 	}
 	if (!isDedicated)
 		TexMgr_FreeTexturesForOwner (mod);
