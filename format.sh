8fbf9a94ebd46a60825e66c81d47abb160180bb8
Axel Gneiting <axelgneiting@gmail.com>
2022-12-08 18:54:02 -0800

    Small fix for format script

--- format.sh
+++ format.sh
@@ -1,2 +1,3 @@
 #/bin/sh
-docker run --user $(id -u):$(id -g) -v `pwd`:`pwd` -w `pwd` -i --rm ghcr.io/jidicula/clang-format:15 -i Quake/*.c Quake/*.h
+docker run --user $(id -u):$(id -g) -v `pwd`:`pwd` -w `pwd` -i --rm ghcr.io/jidicula/clang-format:15 -i Quake/*.c
+docker run --user $(id -u):$(id -g) -v `pwd`:`pwd` -w `pwd` -i --rm ghcr.io/jidicula/clang-format:15 -i Quake/*.h
