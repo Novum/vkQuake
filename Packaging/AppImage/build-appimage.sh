b38a97ea6b159cab835e6fd5f1444205fca17e26
Axel Gneiting <axelgneiting@gmail.com>
2022-12-11 15:48:31 -0800

    Update Linux AppImage to SDL 2.26.1 Also update meson, ninja and linuxdeploy

--- Packaging/AppImage/build-appimage.sh
+++ Packaging/AppImage/build-appimage.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-docker build --tag=build-vkquake docker
+docker build --tag=build-vkquake docker && \
 docker run --rm --privileged -e VERSION=`./get-version.sh` -v ${PWD}/../..:/usr/src/vkQuake build-vkquake /usr/src/vkQuake/Packaging/AppImage/run-in-docker.sh
