520e5feb7a98eb021352851da13628f255c4daaf
Chris Cowan <agentme49@gmail.com>
2023-03-05 04:53:13 -0800

    Fix joystick movement threshold and run behavior
    Fixes issue where holding the movement joystick at a 45 degree angle would
    instead make you move at a 41 degree angle if you had "Always Run" set to
    "Vanilla" or at a 60 degree angle.
    
    Fixes issue where holding the movement joystick all the way while not running
    would make you move at speed 283 instead of 200.
    
    Fixes issue where while running you reach the maximum speed while holding the
    joystick at only 83% (when joy_exponent_move is at its default value of 3, and
    when joy_exponent_move is 2, 75%, and when joy_exponent_move is 1, 57%).
    
    # Conflicts:
    #	Quake/in_sdl.c

--- Quake/in_sdl.c
+++ Quake/in_sdl.c
@@ -320,29 +320,7 @@ static joyaxis_t IN_ApplyEasing (joyaxis_t axis, float exponent)
 
 /*
 ================
-IN_ApplyMoveEasing
 
-same as IN_ApplyEasing, but scales the output by sqrt(2).
-this gives diagonal stick inputs coordinates of (+/-1,+/-1).
-
-forward/back/left/right will return +/- 1.41; this shouldn't be a problem because
-you can pull back on the stick to go slower (and the final speed is clamped
-by sv_maxspeed).
-================
-*/
-static joyaxis_t IN_ApplyMoveEasing (joyaxis_t axis, float exponent)
-{
-	joyaxis_t	result = IN_ApplyEasing (axis, exponent);
-	const float v = sqrtf (2.0f);
-
-	result.x *= v;
-	result.y *= v;
-
-	return result;
-}
-
-/*
-================
 IN_ApplyDeadzone
 
 in: raw joystick axis values converted to floats in +-1
@@ -539,9 +517,10 @@ IN_JoyMove
 */
 void IN_JoyMove (usercmd_t *cmd)
 {
-	float	  speed;
-	joyaxis_t moveRaw, moveDeadzone, moveEased;
-	joyaxis_t lookRaw, lookDeadzone, lookEased;
+	float		  speed;
+	joyaxis_t	  moveRaw, moveDeadzone, moveEased;
+	joyaxis_t	  lookRaw, lookDeadzone, lookEased;
+	extern cvar_t sv_maxspeed;
 
 	if (!joy_enable.value)
 		return;
@@ -567,16 +546,21 @@ void IN_JoyMove (usercmd_t *cmd)
 	moveDeadzone = IN_ApplyDeadzone (moveRaw, joy_deadzone_move.value, joy_outer_threshold_move.value);
 	lookDeadzone = IN_ApplyDeadzone (lookRaw, joy_deadzone_look.value, joy_outer_threshold_look.value);
 
-	moveEased = IN_ApplyMoveEasing (moveDeadzone, joy_exponent_move.value);
+	moveEased = IN_ApplyEasing (moveDeadzone, joy_exponent_move.value);
 	lookEased = IN_ApplyEasing (lookDeadzone, joy_exponent.value);
 
-	if ((in_speed.state & 1) ^ (cl_alwaysrun.value != 0.0))
-		speed = cl_movespeedkey.value;
+	if ((in_speed.state & 1) ^ (cl_alwaysrun.value != 0.0 || cl_forwardspeed.value >= sv_maxspeed.value))
+		// running
+		speed = sv_maxspeed.value;
+	else if (cl_forwardspeed.value >= sv_maxspeed.value)
+		// not running, with always run = vanilla
+		speed = q_min (sv_maxspeed.value, cl_forwardspeed.value / cl_movespeedkey.value);
 	else
-		speed = 1;
+		// not running, with always run = off or quakespasm
+		speed = cl_forwardspeed.value;
 
-	cmd->sidemove += (cl_sidespeed.value * speed * moveEased.x);
-	cmd->forwardmove -= (cl_forwardspeed.value * speed * moveEased.y);
+	cmd->sidemove += speed * moveEased.x;
+	cmd->forwardmove -= speed * moveEased.y;
 
 	if (CL_AngleLocked ())
 		return;
