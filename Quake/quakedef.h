28bc9559c882d1cd8d6595fc67d82b839f5d1df9
Andrei Drexler <makro@rq3.com>
2023-11-25 13:07:58 +0100

    Convert a few more macros to enums to help debugging on MSVC

--- Quake/quakedef.h
+++ Quake/quakedef.h
@@ -122,95 +122,112 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //
 // stats are integers communicated to the client by the server
 //
-#define MAX_CL_STATS	   256
-#define STAT_HEALTH		   0
-#define STAT_FRAGS		   1
-#define STAT_WEAPON		   2
-#define STAT_AMMO		   3
-#define STAT_ARMOR		   4
-#define STAT_WEAPONFRAME   5
-#define STAT_SHELLS		   6
-#define STAT_NAILS		   7
-#define STAT_ROCKETS	   8
-#define STAT_CELLS		   9
-#define STAT_ACTIVEWEAPON  10
-#define STAT_TOTALSECRETS  11
-#define STAT_TOTALMONSTERS 12
-#define STAT_SECRETS	   13 // bumped on client side by svc_foundsecret
-#define STAT_MONSTERS	   14 // bumped by svc_killedmonster
-#define STAT_ITEMS		   15 // replaces clc_clientdata info
-#define STAT_VIEWHEIGHT	   16 // replaces clc_clientdata info
-#define STAT_VIEWZOOM	   21 // DP
-#define STAT_IDEALPITCH	   25 // nq-emu
-#define STAT_PUNCHANGLE_X  26 // nq-emu
-#define STAT_PUNCHANGLE_Y  27 // nq-emu
-#define STAT_PUNCHANGLE_Z  28 // nq-emu
+// clang-format off
+typedef enum
+{
+	MAX_CL_BASE_STATS	= 32,
+	MAX_CL_STATS		= 256,
+
+	STAT_HEALTH			= 0,
+	STAT_FRAGS			= 1,
+	STAT_WEAPON			= 2,
+	STAT_AMMO			= 3,
+	STAT_ARMOR			= 4,
+	STAT_WEAPONFRAME	= 5,
+	STAT_SHELLS			= 6,
+	STAT_NAILS			= 7,
+	STAT_ROCKETS		= 8,
+	STAT_CELLS			= 9,
+	STAT_ACTIVEWEAPON	= 10,
+	STAT_NONCLIENT		= 11,	// first stat not included in svc_clientdata
+	STAT_TOTALSECRETS	= 11,
+	STAT_TOTALMONSTERS	= 12,
+	STAT_SECRETS		= 13,	// bumped on client side by svc_foundsecret
+	STAT_MONSTERS		= 14,	// bumped by svc_killedmonster
+	STAT_ITEMS			= 15,	//replaces clc_clientdata info
+	STAT_VIEWHEIGHT		= 16, // replaces clc_clientdata info
+	STAT_VIEWZOOM		= 21, // DP
+	STAT_IDEALPITCH		= 25, // nq-emu
+	STAT_PUNCHANGLE_X	= 26, // nq-emu
+	STAT_PUNCHANGLE_Y	= 27, // nq-emu
+	STAT_PUNCHANGLE_Z	= 28, // nq-emu
+} stat_t;
 
 // stock defines
 //
-#define IT_SHOTGUN			1
-#define IT_SUPER_SHOTGUN	2
-#define IT_NAILGUN			4
-#define IT_SUPER_NAILGUN	8
-#define IT_GRENADE_LAUNCHER 16
-#define IT_ROCKET_LAUNCHER	32
-#define IT_LIGHTNING		64
-#define IT_SUPER_LIGHTNING	128
-#define IT_SHELLS			256
-#define IT_NAILS			512
-#define IT_ROCKETS			1024
-#define IT_CELLS			2048
-#define IT_AXE				4096
-#define IT_ARMOR1			8192
-#define IT_ARMOR2			16384
-#define IT_ARMOR3			32768
-#define IT_SUPERHEALTH		65536
-#define IT_KEY1				131072
-#define IT_KEY2				262144
-#define IT_INVISIBILITY		524288
-#define IT_INVULNERABILITY	1048576
-#define IT_SUIT				2097152
-#define IT_QUAD				4194304
-#define IT_SIGIL1			(1 << 28)
-#define IT_SIGIL2			(1 << 29)
-#define IT_SIGIL3			(1 << 30)
-#define IT_SIGIL4			(1 << 31)
+typedef enum
+{
+	IT_SHOTGUN			= 1,
+	IT_SUPER_SHOTGUN	= 2,
+	IT_NAILGUN			= 4,
+	IT_SUPER_NAILGUN	= 8,
+	IT_GRENADE_LAUNCHER	= 16,
+	IT_ROCKET_LAUNCHER	= 32,
+	IT_LIGHTNING		= 64,
+	IT_SUPER_LIGHTNING	= 128,
+	IT_SHELLS			= 256,
+	IT_NAILS			= 512,
+	IT_ROCKETS			= 1024,
+	IT_CELLS			= 2048,
+	IT_AXE				= 4096,
+	IT_ARMOR1			= 8192,
+	IT_ARMOR2			= 16384,
+	IT_ARMOR3			= 32768,
+	IT_SUPERHEALTH		= 65536,
+	IT_KEY1				= 131072,
+	IT_KEY2				= 262144,
+	IT_INVISIBILITY		= 524288,
+	IT_INVULNERABILITY	= 1048576,
+	IT_SUIT				= 2097152,
+	IT_QUAD				= 4194304,
+	IT_SIGIL1			= (1<<28),
+	IT_SIGIL2			= (1<<29),
+	IT_SIGIL3			= (1<<30),
+	IT_SIGIL4			= (1<<31),
+} items_t;
 
 //===========================================
 // rogue changed and added defines
 
-#define RIT_SHELLS			   128
-#define RIT_NAILS			   256
-#define RIT_ROCKETS			   512
-#define RIT_CELLS			   1024
-#define RIT_AXE				   2048
-#define RIT_LAVA_NAILGUN	   4096
-#define RIT_LAVA_SUPER_NAILGUN 8192
-#define RIT_MULTI_GRENADE	   16384
-#define RIT_MULTI_ROCKET	   32768
-#define RIT_PLASMA_GUN		   65536
-#define RIT_ARMOR1			   8388608
-#define RIT_ARMOR2			   16777216
-#define RIT_ARMOR3			   33554432
-#define RIT_LAVA_NAILS		   67108864
-#define RIT_PLASMA_AMMO		   134217728
-#define RIT_MULTI_ROCKETS	   268435456
-#define RIT_SHIELD			   536870912
-#define RIT_ANTIGRAV		   1073741824
-#define RIT_SUPERHEALTH		   2147483648
+typedef enum
+{
+	RIT_SHELLS				= 128,
+	RIT_NAILS				= 256,
+	RIT_ROCKETS				= 512,
+	RIT_CELLS				= 1024,
+	RIT_AXE					= 2048,
+	RIT_LAVA_NAILGUN		= 4096,
+	RIT_LAVA_SUPER_NAILGUN	= 8192,
+	RIT_MULTI_GRENADE		= 16384,
+	RIT_MULTI_ROCKET		= 32768,
+	RIT_PLASMA_GUN			= 65536,
+	RIT_ARMOR1				= 8388608,
+	RIT_ARMOR2				= 16777216,
+	RIT_ARMOR3				= 33554432,
+	RIT_LAVA_NAILS			= 67108864,
+	RIT_PLASMA_AMMO			= 134217728,
+	RIT_MULTI_ROCKETS		= 268435456,
+	RIT_SHIELD				= 536870912,
+	RIT_ANTIGRAV			= 1073741824,
+	RIT_SUPERHEALTH			= 2147483648,
+} rogueitems_t;
 
 // MED 01/04/97 added hipnotic defines
 //===========================================
 // hipnotic added defines
-#define HIT_PROXIMITY_GUN_BIT 16
-#define HIT_MJOLNIR_BIT		  7
-#define HIT_LASER_CANNON_BIT  23
-#define HIT_PROXIMITY_GUN	  (1 << HIT_PROXIMITY_GUN_BIT)
-#define HIT_MJOLNIR			  (1 << HIT_MJOLNIR_BIT)
-#define HIT_LASER_CANNON	  (1 << HIT_LASER_CANNON_BIT)
-#define HIT_WETSUIT			  (1 << (23 + 2))
-#define HIT_EMPATHY_SHIELDS	  (1 << (23 + 3))
+typedef enum
+{
+	HIT_PROXIMITY_GUN_BIT	= 16,
+	HIT_MJOLNIR_BIT			= 7,
+	HIT_LASER_CANNON_BIT	= 23,
+	HIT_PROXIMITY_GUN		= (1<<HIT_PROXIMITY_GUN_BIT),
+	HIT_MJOLNIR				= (1<<HIT_MJOLNIR_BIT),
+	HIT_LASER_CANNON		= (1<<HIT_LASER_CANNON_BIT),
+	HIT_WETSUIT				= (1<<(23+2)),
+	HIT_EMPATHY_SHIELDS		= (1<<(23+3)),
+} hipnoticitems_t;
 
+// clang-format on
 //===========================================
 
 #define MAX_SCOREBOARD	   16
