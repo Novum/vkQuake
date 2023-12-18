e8cf9edb1117f9f7ac856cf35c9a54d4439a6c2b
Chris Cowan <agentme49@gmail.com>
2023-12-18 01:29:58 -0800

    CI workflow updates
    Update MacOS CI to MacOS 12.
    
    Update Linux CI to Ubuntu 20.04.
    
    Quoting fixes and style improvements in run-in-docker.sh
    
    Update Github Actions to latest versions.

--- Packaging/AppImage/run-in-docker.sh
+++ Packaging/AppImage/run-in-docker.sh
@@ -1,17 +1,24 @@
-#!/bin/sh
-FOLDER=vkquake-${VERSION}_linux64
-ARCHIVE=${FOLDER}.tar.gz
+#!/bin/bash
+set -euo pipefail
 
-cd /usr/src/vkQuake \
-  && rm -rf build/appimage \
-  && ./Packaging/AppImage/python3.11.0.AppImage /opt/meson/meson.py build/appimage -Dbuildtype=release -Db_lto=true \
-  && ninja -C build/appimage \
-  && cd Packaging/AppImage \
-  && rm -rf AppDir \
-  && rm -rf vkquake* \
-  && mkdir ${FOLDER} \
-  && ./linuxdeploy-x86_64.AppImage -e ../../build/appimage/vkquake --appdir=AppDir --create-desktop-file \
-     -i ../../Misc/vkQuake_256.png --icon-filename=vkquake --output appimage \
-  && cp vkquake-${VERSION}-x86_64.AppImage ${FOLDER}/vkquake.AppImage \
-  && cp ../../LICENSE.txt ${FOLDER} \
-  && tar -zcvf ${ARCHIVE} ${FOLDER}
+FOLDER=vkquake-${VERSION}_linux64
+ARCHIVE=$FOLDER.tar.gz
+
+cd /usr/src/vkQuake
+
+rm -rf build/appimage
+
+python3 /opt/meson/meson.py build/appimage -Dbuildtype=release -Db_lto=true
+ninja -C build/appimage
+
+cd Packaging/AppImage
+rm -rf AppDir
+rm -rf vkquake*
+mkdir "$FOLDER"
+./linuxdeploy-x86_64.AppImage \
+	-e ../../build/appimage/vkquake --appdir=AppDir --create-desktop-file \
+	-i ../../Misc/vkQuake_256.png --icon-filename=vkquake --output appimage
+
+cp "vkquake-$VERSION-x86_64.AppImage" "$FOLDER/vkquake.AppImage"
+cp ../../LICENSE.txt "$FOLDER"
+tar -zcvf "$ARCHIVE" "$FOLDER"
