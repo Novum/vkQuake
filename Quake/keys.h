dbba9283783bd589801fed6dbb27b001ced944bb
Andrei Drexler <makro@rq3.com>
2023-12-30 11:13:01 +0100

    Avoid using explicit keycode values to make it easier to add keys in the future

--- Quake/keys.h
+++ Quake/keys.h
@@ -38,114 +38,116 @@ typedef enum keycode_t
 // normal keys should be passed as lowercased ascii
 
 	K_BACKSPACE			= 127,
-	K_UPARROW			= 128,
-	K_DOWNARROW			= 129,
-	K_LEFTARROW			= 130,
-	K_RIGHTARROW		= 131,
+	K_UPARROW,
+	K_DOWNARROW,
+	K_LEFTARROW,
+	K_RIGHTARROW,
 
-	K_ALT				= 132,
-	K_CTRL				= 133,
-	K_SHIFT				= 134,
-	K_F1				= 135,
-	K_F2				= 136,
-	K_F3				= 137,
-	K_F4				= 138,
-	K_F5				= 139,
-	K_F6				= 140,
-	K_F7				= 141,
-	K_F8				= 142,
-	K_F9				= 143,
-	K_F10				= 144,
-	K_F11				= 145,
-	K_F12				= 146,
-	K_INS				= 147,
-	K_DEL				= 148,
-	K_PGDN				= 149,
-	K_PGUP				= 150,
-	K_HOME				= 151,
-	K_END				= 152,
+	K_ALT,
+	K_CTRL,
+	K_SHIFT,
+	K_F1,
+	K_F2,
+	K_F3,
+	K_F4,
+	K_F5,
+	K_F6,
+	K_F7,
+	K_F8,
+	K_F9,
+	K_F10,
+	K_F11,
+	K_F12,
+	K_INS,
+	K_DEL,
+	K_PGDN,
+	K_PGUP,
+	K_HOME,
+	K_END,
 
-	K_KP_NUMLOCK		= 153,
-	K_KP_SLASH			= 154,
-	K_KP_STAR			= 155,
-	K_KP_MINUS			= 156,
-	K_KP_HOME			= 157,
-	K_KP_UPARROW		= 158,
-	K_KP_PGUP			= 159,
-	K_KP_PLUS			= 160,
-	K_KP_LEFTARROW		= 161,
-	K_KP_5				= 162,
-	K_KP_RIGHTARROW		= 163,
-	K_KP_END			= 164,
-	K_KP_DOWNARROW		= 165,
-	K_KP_PGDN			= 166,
-	K_KP_ENTER			= 167,
-	K_KP_INS			= 168,
-	K_KP_DEL			= 169,
+	K_KP_NUMLOCK,
+	K_KP_SLASH,
+	K_KP_STAR,
+	K_KP_MINUS,
+	K_KP_HOME,
+	K_KP_UPARROW,
+	K_KP_PGUP,
+	K_KP_PLUS,
+	K_KP_LEFTARROW,
+	K_KP_5,
+	K_KP_RIGHTARROW,
+	K_KP_END,
+	K_KP_DOWNARROW,
+	K_KP_PGDN,
+	K_KP_ENTER,
+	K_KP_INS,
+	K_KP_DEL,
 
-	K_COMMAND			= 170,
+	K_COMMAND,
 
-	K_CAPSLOCK			= 171,
-	K_SCROLLLOCK		= 172,
-	K_PRINTSCREEN		= 173,
-
-	K_PAUSE				= 255,
+	K_CAPSLOCK,
+	K_SCROLLLOCK,
+	K_PRINTSCREEN,
 
 //
 // mouse buttons generate virtual keys
 //
 	K_MOUSE1			= 200,
-	K_MOUSE2			= 201,
-	K_MOUSE3			= 202,
-
-// JACK: Intellimouse(c) Mouse Wheel Support
-
-	K_MWHEELUP			= 239,
-	K_MWHEELDOWN		= 240,
+	K_MOUSE2,
+	K_MOUSE3,
 
 // thumb buttons
-	K_MOUSE4			= 241,
-	K_MOUSE5			= 242,
+	K_MOUSE4,
+	K_MOUSE5,
+
+// JACK: Intellimouse(c) Mouse Wheel Support
+	K_MWHEELUP,
+	K_MWHEELDOWN,
 
 // SDL2 game controller keys
-	K_LTHUMB			= 243,
-	K_RTHUMB			= 244,
-	K_LSHOULDER			= 245,
-	K_RSHOULDER			= 246,
-	K_ABUTTON			= 247,
-	K_BBUTTON			= 248,
-	K_XBUTTON			= 249,
-	K_YBUTTON			= 250,
-	K_LTRIGGER			= 251,
-	K_RTRIGGER			= 252,
+	K_LTHUMB,
+	K_RTHUMB,
+	K_LSHOULDER,
+	K_RSHOULDER,
+	K_ABUTTON,
+	K_BBUTTON,
+	K_XBUTTON,
+	K_YBUTTON,
+	K_LTRIGGER,
+	K_RTRIGGER,
+
+	K_PAUSE,
+
+	NUM_KEYCODES,
 } keycode_t;
 
 // clang-format on
 
-#define MAX_KEYS 256
+#define	MAX_KEYS		256
 
-#define MAXCMDLINE 256
+#define	MAXCMDLINE	256
 
-typedef enum
+typedef enum {key_game, key_console, key_message, key_menu} keydest_t;
+typedef enum textmode_t
 {
-	key_game,
-	key_console,
-	key_message,
-	key_menu
-} keydest_t;
+	TEXTMODE_OFF,		// no char events
+	TEXTMODE_ON,		// char events, show on-screen keyboard
+	TEXTMODE_NOPOPUP,	// char events, don't show on-screen keyboard
+} textmode_t;
 
-extern keydest_t key_dest;
-extern char		*keybindings[MAX_KEYS];
+extern keydest_t	key_dest;
+extern	char	*keybindings[MAX_KEYS];
 
-#define CMDLINES 64
+#define		CMDLINES 64
 
-extern char	  key_lines[CMDLINES][MAXCMDLINE];
-extern int	  edit_line;
-extern int	  key_linepos;
-extern int	  key_insert;
-extern double key_blinktime;
+extern	char	key_lines[CMDLINES][MAXCMDLINE];
+extern	char	key_tabhint[MAXCMDLINE];
+extern	int		edit_line;
+extern	int		key_linepos;
+extern	int		key_insert;
+extern	double		key_blinktime;
 
-extern qboolean chat_team;
+extern	qboolean	chat_team;
 
 void Key_Init (void);
 void Key_ClearStates (void);
@@ -155,20 +157,22 @@ void Key_BeginInputGrab (void);
 void Key_EndInputGrab (void);
 void Key_GetGrabbedInput (int *lastkey, int *lastchar);
 
-void	 Key_Event (int key, qboolean down);
-void	 Key_EventWithKeycode (int key, qboolean down, int keycode);
-void	 Char_Event (int key);
-qboolean Key_TextEntry (void);
+void Key_Event (int key, qboolean down);
+void Key_EventWithKeycode (int key, qboolean down, int keycode);
+void Char_Event (int key);
+textmode_t Key_TextEntry (void);
 
-void		Key_SetBinding (int keynum, const char *binding);
+void Key_SetBinding (int keynum, const char *binding);
+int Key_GetKeysForCommand (const char *command, int *keys, int maxkeys);
 const char *Key_KeynumToString (int keynum);
-void		Key_WriteBindings (FILE *f);
+void Key_WriteBindings (FILE *f);
 
-void		Key_EndChat (void);
+void Key_EndChat (void);
 const char *Key_GetChatBuffer (void);
-int			Key_GetChatMsgLen (void);
+int Key_GetChatMsgLen (void);
 
 void History_Init (void);
 void History_Shutdown (void);
 
-#endif /* _QUAKE_KEYS_H */
+#endif	/* _QUAKE_KEYS_H */
+
