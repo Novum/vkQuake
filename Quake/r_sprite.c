sezero/quakespasm

Backport angled sprites code from FTEQW

Quake/r_sprite.c
@@ -49,6 +49,19 @@ mspriteframe_t *R_GetSpriteFrame (entity_t *currentent)
 	{
 		pspriteframe = psprite->frames[frame].frameptr;
 	}
+	else if (psprite->frames[frame].type == SPR_ANGLED)
+	{
+		// erysdren - angled sprites code backported from FTEQW
+		vec3_t axis[3];
+		AngleVectors(currententity->angles, axis[0], axis[1], axis[2]);
+		{
+			float f = DotProduct(vpn, axis[0]);
+			float r = DotProduct(vright, axis[0]);
+			int dir = (atan2(r, f)+1.125*M_PI)*(4/M_PI);
+			pspritegroup = (mspritegroup_t *)psprite->frames[frame].frameptr;
+			pspriteframe = pspritegroup->frames[dir&7];
+		}
+	}
 	else
 	{
 		pspritegroup = (mspritegroup_t *)psprite->frames[frame].frameptr;

Novum/vkQuake

Quake/r_sprite.c
@@ -52,6 +52,19 @@ static mspriteframe_t *R_GetSpriteFrame (entity_t *currentent)
 	{
 		pspriteframe = psprite->frames[frame].frameptr;
 	}
+	else if (psprite->frames[frame].type == SPR_ANGLED)
+	{
+		// erysdren - angled sprites code backported from FTEQW
+		vec3_t axis[3];
+		AngleVectors(currentent->angles, axis[0], axis[1], axis[2]);
+		{
+			float f = DotProduct(vpn, axis[0]);
+			float r = DotProduct(vright, axis[0]);
+			int dir = (atan2(r, f)+1.125*M_PI)*(4/M_PI);
+			pspritegroup = (mspritegroup_t *)psprite->frames[frame].frameptr;
+			pspriteframe = pspritegroup->frames[dir&7];
+		}
+	}
 	else
 	{
 		pspritegroup = (mspritegroup_t *)psprite->frames[frame].frameptr;