b4cfa920584900e2315656449dddff9a576ad5f6
vsonnier <vsonnier@users.noreply.github.com>
2024-06-27 22:09:45 +0200

    Fix #699 : Updated Clang Format to 17, update SDL Dovcker image to 2.30.4, Vulkan to 1.3.275

--- format.sh
+++ format.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-docker run --user $(id -u):$(id -g) -v `pwd`:`pwd` -w `pwd` -i --rm ghcr.io/jidicula/clang-format:15 -i Quake/*.c Quake/*.h Shaders/*.frag Shaders/*.vert Shaders/*.comp Shaders/*.inc Shaders/*.c Shaders/*.h
+docker run --user $(id -u):$(id -g) -v `pwd`:`pwd` -w `pwd` -i --rm ghcr.io/jidicula/clang-format:17 -i Quake/*.c Quake/*.h Shaders/*.frag Shaders/*.vert Shaders/*.comp Shaders/*.inc Shaders/*.c Shaders/*.h
