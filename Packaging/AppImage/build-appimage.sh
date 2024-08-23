d89bcb289a1ad0a9106007882254d90bb067e0cf
vsonnier <vsonnier@users.noreply.github.com>
2024-08-23 22:33:59 +0200

    Another fix for #722

--- Packaging/AppImage/build-appimage.sh
+++ Packaging/AppImage/build-appimage.sh
@@ -1,3 +1,4 @@
 #/bin/sh
+VERSION=`sh get-version.sh`
 docker build --tag=build-vkquake docker && \
-docker run --rm --privileged -e VERSION=`./get-version.sh` -v ${PWD}/../..:/usr/src/vkQuake build-vkquake /usr/src/vkQuake/Packaging/AppImage/run-in-docker.sh
+docker run --rm --privileged --env VERSION=${VERSION} -v ${PWD}/../..:/usr/src/vkQuake build-vkquake /usr/src/vkQuake/Packaging/AppImage/run-in-docker.sh
