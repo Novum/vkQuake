1c1fe525e26fdd66ed95e5418ea29d0d0646f1de
vsonnier <vsonnier@users.noreply.github.com>
2024-08-23 22:06:39 +0200

    Fix #722 : version numbers are fetched in quakever.h now

--- Packaging/AppImage/get-version.sh
+++ Packaging/AppImage/get-version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-BASE_VERSION=`grep -oP "^#define\s*VKQUAKE_VERSION\s*\K[0-9.]*" ../../Quake/quakedef.h`
-PATCH_VERSION=`grep -oP "^#define\s*VKQUAKE_VER_PATCH\s*\K[0-9.]*" ../../Quake/quakedef.h`
-SUFFIX=`grep -oP "^#define\s*VKQUAKE_VER_SUFFIX\s*\"\K([^\"]*)" ../../Quake/quakedef.h`
+BASE_VERSION=`grep -oP "^#define\s*VKQUAKE_VERSION\s*\K[0-9.]*" ../../Quake/quakever.h`
+PATCH_VERSION=`grep -oP "^#define\s*VKQUAKE_VER_PATCH\s*\K[0-9.]*" ../../Quake/quakever.h`
+SUFFIX=`grep -oP "^#define\s*VKQUAKE_VER_SUFFIX\s*\"\K([^\"]*)" ../../Quake/quakever.h`
 echo ${BASE_VERSION}.${PATCH_VERSION}${SUFFIX}
