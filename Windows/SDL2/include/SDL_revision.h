d5f8694e7bbdfeb0d61e3333a2403a182827371d
Ozkan Sezer <sezeroz@gmail.com>
2024-11-02 23:33:50 +0300

    update SDL2 to latest official 2.30.9 version

--- Windows/SDL2/include/SDL_revision.h
+++ Windows/SDL2/include/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.30.8 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-2.30.9 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-2.30.8"
+#define SDL_REVISION "SDL-2.30.9"
 #endif
