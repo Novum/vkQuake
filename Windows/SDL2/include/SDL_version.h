7e71dcd5f0383ce7ab30ddb1b85c421fb48b2d77
Ozkan Sezer <sezeroz@gmail.com>
2025-01-07 11:01:40 +0300

    update windows SDL2 to latest official 2.30.11 version.

--- Windows/SDL2/include/SDL_version.h
+++ Windows/SDL2/include/SDL_version.h
@@ -1,6 +1,6 @@
 /*
   Simple DirectMedia Layer
-  Copyright (C) 1997-2024 Sam Lantinga <slouken@libsdl.org>
+  Copyright (C) 1997-2025 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -59,7 +59,7 @@ typedef struct SDL_version
 */
 #define SDL_MAJOR_VERSION   2
 #define SDL_MINOR_VERSION   30
-#define SDL_PATCHLEVEL      10
+#define SDL_PATCHLEVEL      11
 
 /**
  * Macro to determine SDL version program was compiled against.
