209632185d3c5e49193730a2b249100a4ef05be0
vsonnier <vsonnier@users.noreply.github.com>
2025-02-28 17:51:47 +0100

    Boost MAX_MODELS to 8192 because 4096 limit was getting close on some levels, boost MAX_SOUNDS also
    (Because models and sounds are cached **once** per new Server connection)

--- Quake/quakedef.h
+++ Quake/quakedef.h
@@ -92,8 +92,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // ents past 8192 can't play sounds in the standard protocol
 #define MAX_LIGHTSTYLES	  64
-#define MAX_MODELS		  4096 // johnfitz -- was 256
-#define MAX_SOUNDS		  2048 // johnfitz -- was 256
+#define MAX_MODELS		  8192 // johnfitz -- was 256
+#define MAX_SOUNDS		  4096 // johnfitz -- was 256
 #define MAX_PARTICLETYPES 2048
 
 #define SAVEGAME_COMMENT_LENGTH 39
