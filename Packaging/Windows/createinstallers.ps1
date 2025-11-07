5827e9e2e2cb65db815a95764bfc3f6038503992
vsonnier <vsonnier@users.noreply.github.com>
2025-11-07 16:41:02 +0100

    Discussion #815: Remove 32bit build support

--- Packaging/Windows/createinstallers.ps1
+++ Packaging/Windows/createinstallers.ps1
@@ -8,7 +8,6 @@ $SuffixMatch=Select-String "^#define\s*VKQUAKE_VER_SUFFIX\s*`"([^`"]*)" "../../Q
 $Suffix=$SuffixMatch.Matches.groups[1].value
 $Version=$VersionMajor + '.' + $VersionMinor + '.' + $PatchVersion + $Suffix
 $SrcDirX64="..\..\Windows\VisualStudio\Build-vkQuake\x64\Release"
-$SrcDirX86="..\..\Windows\VisualStudio\Build-vkQuake\x86\Release"
 
 # Cleanup
 Del "vkQuake-*.exe"
@@ -16,13 +15,10 @@ Del "vkQuake-*.zip"
 
 # Clean & build binaries
 msbuild ..\..\Windows\VisualStudio\vkquake.sln /target:Clean /target:Build /property:Configuration=Release /property:Platform=x64
-msbuild ..\..\Windows\VisualStudio\vkquake.sln /target:Clean /target:Build /property:Configuration=Release /property:Platform=Win32
 
 # Create NSIS exe installers
 $Nsis="C:\Program Files (x86)\NSIS\Bin\makensis.exe"
 $NsisArguments = '-V4 -DSRCDIR="' + $SrcDirX64 + '" -DPLATFORM=x64 -DVERSION=' + $Version + ' vkQuake.nsi'
-Start-Process -Wait -NoNewWindow -PassThru -FilePath $Nsis -ArgumentList $NsisArguments
-$NsisArguments = '-V4 -DSRCDIR="' + $SrcDirX86 + '" -DPLATFORM=x86 -DVERSION=' + $Version + ' vkQuake.nsi'
 Start-Process -Wait -NoNewWindow -PassThru -FilePath $Nsis -ArgumentList $NsisArguments
 
 # Create zip files
@@ -30,11 +26,5 @@ $compress = @{
   Path = "$SrcDirX64\*.exe", "$SrcDirX64\*.dll", "..\..\LICENSE.txt"
   CompressionLevel = "Optimal"
   DestinationPath = "vkQuake-" + $Version + "_win64.zip"
-}
-Compress-Archive @compress
-$compress = @{
-  Path = "$SrcDirX86\*.exe", "$SrcDirX86\*.dll", "..\..\LICENSE.txt"
-  CompressionLevel = "Optimal"
-  DestinationPath = "vkQuake-" + $Version + "_win32.zip"
 }
 Compress-Archive @compress
