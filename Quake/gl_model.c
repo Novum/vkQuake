6f9c012a9176ee36663fd7a6d30b6291a0dbbcc1
vsonnier <vsonnier@users.noreply.github.com>
2025-11-18 20:49:35 +0100

    Fix bad parenthesis

--- Quake/gl_model.c
+++ Quake/gl_model.c
@@ -1767,11 +1767,11 @@ static void Mod_LoadFaces (qmodel_t *mod, byte *mod_base, lump_t *l, qboolean bs
 				out->flags |= SURF_DRAWTILED; // unlit water
 
 			// detect special liquid types
-			if (!(strncmp (out->texinfo->texture->name, "*lava", 5) || strncmp (out->texinfo->texture->name, "!lava", 5)))
+			if (!strncmp (out->texinfo->texture->name, "*lava", 5) || !strncmp (out->texinfo->texture->name, "!lava", 5))
 				out->flags |= SURF_DRAWLAVA;
-			else if (!(strncmp (out->texinfo->texture->name, "*slime", 6) || strncmp (out->texinfo->texture->name, "!slime", 6)))
+			else if (!strncmp (out->texinfo->texture->name, "*slime", 6) || !strncmp (out->texinfo->texture->name, "!slime", 6))
 				out->flags |= SURF_DRAWSLIME;
-			else if (!(strncmp (out->texinfo->texture->name, "*tele", 5) || strncmp (out->texinfo->texture->name, "!tele", 5)))
+			else if (!strncmp (out->texinfo->texture->name, "*tele", 5) || !strncmp (out->texinfo->texture->name, "!tele", 5))
 				out->flags |= SURF_DRAWTELE;
 			else
 				out->flags |= SURF_DRAWWATER;
