de11770190e904040c66a5157be5129ed0b9aceb
Axel Gneiting <axelgneiting@gmail.com>
2026-09-25 21:06:27 -0700

    Don't write .oldorigin for noclip clients

--- Quake/sv_phys.c
+++ Quake/sv_phys.c
@@ -2292,8 +2292,6 @@ static void SV_Physics_Client (edict_t *ent, int num)
 		if (!SV_RunThink (ent))
 			goto done;
 		VectorMA (ent->v.origin, host_frametime, ent->v.velocity, ent->v.origin);
-		if (!SV_TestEntityPosition (ent))
-			VectorCopy (ent->v.origin, ent->v.oldorigin);
 		break;
 
 	default:
